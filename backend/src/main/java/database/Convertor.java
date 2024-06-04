ed79cd61e14f5a6e72d9ada48ba5b55df359ff81
nforoutann <nazaninforoutan6@gmail.com>
2024-06-04 11:30:15 +0330

    table and convertor classes created

--- backend/src/main/java/database/Convertor.java
+++ backend/src/main/java/database/Convertor.java
@@ -1,4 +1,17 @@
 package database;
 
+import java.util.HashMap;
+
 public class Convertor {
+    public static HashMap<String, String > stringToMap (String s) {
+        String [] info = s.split(",,");
+        HashMap <String, String> res = new HashMap<>();
+        for (var each : info) {
+            int colon = each.indexOf(':');
+            String key = each.substring(0, colon);
+            String value = each.substring(colon + 1);
+            res.put(key, value);
+        }
+        return res;
+    }
 }
--- backend/src/main/java/database/Table.java
+++ backend/src/main/java/database/Table.java
@@ -1,4 +1,36 @@
 package database;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Table {
+    private String studentPath;
+    private String teacherPath;
+    public Table(String studentPath, String teacherPath) {
+        this.studentPath = studentPath;
+        this.teacherPath = teacherPath;
+    }
+    public ArrayList<HashMap <String, String>> get(String role) throws Exception {
+        File file=null;
+        switch (role) {
+            case "student": file = new File(studentPath); break;
+            case "teacher": file = new File(teacherPath); break;
+            default: throw new Exception("Invalid role");
+        }
+        try {
+            Scanner sc = new Scanner(file);
+            ArrayList<HashMap<String, String>> res = new ArrayList<>();
+            while (sc.hasNextLine())
+                res.add(Convertor.stringToMap(sc.nextLine()));
+            sc.close();
+            return res;
+        }
+        catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+    
 }
