af8117346dd7d7291b8095565725ae07694d4229
nforoutann <nazaninforoutan6@gmail.com>
2024-06-10 16:10:21 +0330

    feat: logo and unisphere acceptable location

--- myFront/lib/screens/welcome-screen.dart
+++ myFront/lib/screens/welcome-screen.dart
@@ -11,19 +11,33 @@ class welcomeScreen extends StatelessWidget{
         children: [
           Expanded(
             child: Transform.translate(
-              offset: const Offset(-20.0, -300.0), // Adjust these values to move the photo
+              offset: const Offset(-60.0, -140.0), // Adjust these values to move the photo
               child: ColorFiltered(
                 colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcATop),
                 child: SizedBox(
-                  height: 100,
                   width: 300,
+                  height: 200,
                   child: Image.asset(
-                    'assets/images/logoWritten.png',
+                    'assets/images/logo.png',
                   ),
                 ),
               ),
             ),
-          )
+          ),
+          Expanded(
+            child: Transform.translate(
+              offset: const Offset(100.0, -600.0), // Adjust these values to move the photo
+              child: ColorFiltered(
+                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcATop),
+                child: SizedBox(
+                  width: 300,
+                  child: Image.asset(
+                    'assets/images/unisphere.png',
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
