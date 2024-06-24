0daabf4160c67a324a206658d29aa4d28cabc3b6
nforoutann <nazaninforoutan6@gmail.com>
2024-06-24 18:50:19 +0330

    feat: more changes on database and data files plus adding new screens to thefront and a little completed them

---
+++ frontend/lib/screens/student-classes-screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClassScreen extends StatefulWidget{
+  ClassScreen({super.key});
+
+  @override
+  State<ClassScreen> createState() => _ClassScreenState();
+}
+
+class _ClassScreenState extends State<ClassScreen> {
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
---
+++ frontend/lib/screens/studnet-news-screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsScreen extends StatefulWidget{
+  NewsScreen({super.key});
+
+  @override
+  State<NewsScreen> createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
